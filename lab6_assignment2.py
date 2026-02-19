7f355723133a5ef101c00426d04f5abe8e914665
TaliBerzins <tali.berzins@outlook.com>
2026-02-19 14:55:37 -0500

    Created wrong password messages and user name message

--- lab6_assignment2.py
+++ lab6_assignment2.py
@@ -1,9 +1,25 @@
+#Creating dictonary for username, pwd pairs
 user_dict = {"guest": "guest", 'TAB': 'pwd', 'tberzins': 'ri74', 'gwalters': 'mango'}
 user_username_input = input("Enter your username:")
-if user_username_input in user_dict.keys():
-    print(user_username_input)
+#Checking if pwd's and usernames entered by user match
+if user_username_input in user_dict.keys() and user_username_input == 'guest':
     user_pwd_input = input("Enter your password:")
     if user_pwd_input == user_dict[user_username_input]:
-        print("You printed correct pwd")
+        print("You have guest acess")
+    elif user_pwd_input != user_dict[user_username_input]:
+         print("Wrong password")
+#Creating case for if username is not guest username
+elif user_username_input in user_dict.keys():
+     user_pwd_input = input("Enter your password:")
+     if user_pwd_input == user_dict[user_username_input]:
+             print(f"Welcome {user_username_input}. You have level 1 access")
+     elif user_pwd_input != user_dict[user_username_input]:
+         print("Wrong password")
+
+else:
+     user_username_input = input("No such userName, enter again")
+   
+
+
 
 
